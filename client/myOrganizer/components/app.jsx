56f68f11b4239a29f825d896ef03d0c1c0cfcaf5
bmdarshan <darshan.bmg@gmail.com>
2017-05-25 12:11:50 -0400

    integrating the react components

---
+++ client/myOrganizer/components/app.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default class App extends React.Component {
+  render() {
+    return (
+     <div style={{textAlign: 'center'}}>
+        <h1>My Organizer</h1>
+      </div>);
+  }
+}
